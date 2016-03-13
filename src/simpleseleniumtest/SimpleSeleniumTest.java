972e8e373cbd8ad71ddaa46f3bfc94946c470fa7
AlexGT <galext@gmail.com>
2016-03-13 15:07:44 +0200

    another unnecessary import deleted

--- src/simpleseleniumtest/SimpleSeleniumTest.java
+++ src/simpleseleniumtest/SimpleSeleniumTest.java
@@ -8,7 +8,6 @@ package simpleseleniumtest;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
