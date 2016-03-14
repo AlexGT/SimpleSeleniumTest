925100080ed23ce6833be2d1b02b846cb72312a1
a_belchikov <a_belchikov@wargaming.net>
2016-03-14 18:33:33 +0200

    CacheLookup removed from factory

--- test/pages/SitePage.java
+++ test/pages/SitePage.java
@@ -7,7 +7,6 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 
 /**
