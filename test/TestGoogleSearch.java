41e483a510fb4287a664b184a1e09422ccb770fa
a_belchikov <a_belchikov@wargaming.net>
2016-03-14 16:49:58 +0200

    page object creating removed from test

--- test/TestGoogleSearch.java
+++ test/TestGoogleSearch.java
@@ -4,8 +4,6 @@
  * and open the template in the editor.
  */
 import org.junit.*;
-import pages.SearchPage;
-import pages.SitePage;
 
 /**
  *
@@ -15,10 +13,8 @@ public class TestGoogleSearch extends BaseTest{
     
     @Test
     public void testSearch() throws InterruptedException{
-        searchPage = new SearchPage(driver);
         searchPage.searchText("Simple Selenium Test");
         
-        resultPage = new SitePage(driver);
         Assert.assertEquals("Creating and running a simple Selenium WebDriver test", resultPage.getHeader());
         System.out.println(resultPage.getHeader());
         System.out.println(resultPage.getUrl());
